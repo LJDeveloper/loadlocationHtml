2d345198ca4068e3ad7026883b2990784b279bb0
jingdong521 <563674780@qq.com>
2017-01-14 10:08:13 +0800

    Initial Commit

---
+++ loadLocationHtml/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  loadLocationHtml
+//
+//  Created by LJ on 17/1/14.
+//  Copyright © 2017年 沸腾医疗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ loadLocationHtml/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  loadLocationHtml
+//
+//  Created by LJ on 17/1/14.
+//  Copyright © 2017年 沸腾医疗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
